d3d5ccf61f13984ac1c4cdff37f6e8899750078b
Samridh Chawla <samridhchawla95@gmail.com>
2024-03-15 15:09:57 -0400

    error fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,66 @@
 /** @format */
 
-import PageTitle from "@/components/PageTitle";
+import PageTitle from "../components/PageTitle";
 import Image from "next/image";
 import { DollarSign, Users, CreditCard, Activity } from "lucide-react";
-import Card, { CardContent, CardProps } from "@/components/Card";
-import BarChart from "@/components/BarChart";
-import SalesCard, { SalesProps } from "@/components/SalesCard";
+import Card, { CardContent, CardProps } from "../components/Card";
+import BarChart from "../components/BarChart";
+import SalesCard, { SalesProps } from "../components/SalesCard";
+import React from "react";
 
 const cardData: CardProps[] = [
   {
     label: "Total Revenue",
     amount: "$45,231.89",
     discription: "+20.1% from last month",
-    icon: DollarSign
+    icon: DollarSign,
   },
   {
     label: "Subscriptions",
     amount: "+2350",
     discription: "+180.1% from last month",
-    icon: Users
+    icon: Users,
   },
   {
     label: "Sales",
     amount: "+12,234",
     discription: "+19% from last month",
-    icon: CreditCard
+    icon: CreditCard,
   },
   {
     label: "Active Now",
     amount: "+573",
     discription: "+201 since last hour",
-    icon: Activity
-  }
+    icon: Activity,
+  },
 ];
 
 const uesrSalesData: SalesProps[] = [
   {
     name: "Olivia Martin",
     email: "olivia.martin@email.com",
-    saleAmount: "+$1,999.00"
+    saleAmount: "+$1,999.00",
   },
   {
     name: "Jackson Lee",
     email: "isabella.nguyen@email.com",
-    saleAmount: "+$1,999.00"
+    saleAmount: "+$1,999.00",
   },
   {
     name: "Isabella Nguyen",
     email: "isabella.nguyen@email.com",
-    saleAmount: "+$39.00"
+    saleAmount: "+$39.00",
   },
   {
     name: "William Kim",
     email: "will@email.com",
-    saleAmount: "+$299.00"
+    saleAmount: "+$299.00",
   },
   {
     name: "Sofia Davis",
     email: "sofia.davis@email.com",
-    saleAmount: "+$39.00"
-  }
+    saleAmount: "+$39.00",
+  },
 ];
 
 export default function Home() {
